f96c0a5c98280a071e8d8b898839bcf90d4c2e6a
Pavel <pavel23578@gmail.com>
2024-01-05 23:08:10 +0300

    ping-pong game added

--- funcs/get_room_by_type.py
+++ funcs/get_room_by_type.py
@@ -1,4 +1,4 @@
-from sprites import SpawnRoom, Terminal1Room, Terminal1RoomMaxwellCat
+from sprites import SpawnRoom, Terminal1Room, Terminal1RoomMaxwellCat, Terminal1RoomPingPong
 
 
 def get_room_by_type(type: str, screen, player, exits=None):
@@ -17,3 +17,5 @@ def get_room_by_type(type: str, screen, player, exits=None):
         return Terminal1Room(screen, player, exits)
     if nt == 'terminal1room_maxwell_cat':
         return Terminal1RoomMaxwellCat(screen, player, exits, compl=('_compl' in type))
+    if nt == 'terminal1room_ping_pong':
+        return Terminal1RoomPingPong(screen, player, exits, compl=('_compl' in type))
